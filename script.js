b02d8ecdda463b84d5004d5d05a2f5138754dcc3
Umut Alptekin <amacalptekin@gmail.com>
2020-10-02 21:54:03 +0300

    creating variables for buttons on js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var apiK = "ixNkGGhn0pHP8XSkvLam0NNRgRimeipV";
 //query url for the first ten results                                        search term                year of publication                                           first ten results            API key                           
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&facet_fields=pub_year&facet=true" + "&begin_date=" + startyeardiv + + "&page=0" + "&api-key=" + apiK;
+var searchBtn = $("#search")
+
 
 $.ajax({
     url: queryURL,
@@ -8,3 +10,5 @@ $.ajax({
 }).then(function (response) {
     console.log(response)
 })
+
+//
