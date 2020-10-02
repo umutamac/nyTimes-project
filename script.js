0cc8c70c2c3f635b66a59cdc0e6e8fc7e6a4b159
Julian Rivera Jr <julianriverajr@gmail.com>
2020-10-02 14:00:33 -0500

    small changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var apiK = "ixNkGGhn0pHP8XSkvLam0NNRgRimeipV";
-//query url for the first ten results                                        search term                year of publication                                           first ten results            API key                           
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&facet_fields=pub_year&facet=true" + "&begin_date=" + startyeardiv + + "&page=0" + "&api-key=" + apiK;
+var 
+//query url for the first ten results                                        search term                year of publication                                                                   first ten results        API key                           
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&facet_fields=pub_year&facet=true" + "&begin_date=" + startyeardiv + "0101&end_date=" + endyeardiv + "&page=0" + "&api-key=" + apiK;
 
 $.ajax({
     url: queryURL,
