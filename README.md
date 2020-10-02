4ca85baef3377d765908530afb2a160fc9480b15
Julian Rivera Jr <julianriverajr@gmail.com>
2020-10-02 14:24:23 -0500

    we worked on buttons, url, and html, among other things

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nyTimes-project
+
+This project is aimed at searching articles on NY Times and display them on our site using API key. There are search parameters that user needs to fill in to be able to do it. 
--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 var apiK = "ixNkGGhn0pHP8XSkvLam0NNRgRimeipV";
-var 
-//query url for the first ten results                                        search term                year of publication                                                                   first ten results        API key                           
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&facet_fields=pub_year&facet=true" + "&begin_date=" + startyeardiv + "0101&end_date=" + endyeardiv + "&page=0" + "&api-key=" + apiK;
+var startYearInput = $("#startYear").val();
+var endYearInput = $("#endYear").val();
+var searchInput = $("#searchTerm").attr("value");
+var numOfArticles = $("option").attr("value");
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response)
-})
+$(document).ready(function () {
+    //query url for the first ten results                                        search term                year of publication                                                                   first ten results        API key                           
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchInput + "&facet_fields=pub_year&facet=true" + "&begin_date=" + startYearInput + "0101&end_date=" + endYearInput + "&page=0" + "&api-key=" + apiK;
 
-//
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+    })
+
+
+    // var searchBtn = $("#search");
+
+    var clearBtn = $("#clear");
+    console.log(clearBtn);
+    clearBtn.click(function () {
+        searchInput = "";
+        startYearInput = "";
+        endYearInput = "";
+        console.log(searchInput);
+        console.log(startYearInput);
+        console.log(endYearInput);
+
+    })
+    //
+
+    for (i = 0; i < numOfArticles; i++) {
+        $(".results").append($("<div>").addClass("articleContent"));
+        $(".articleContent").text(response.Article); // <-- will need to be rechecked    
+    }
+});
